93d1ea3f460c9a92ba68430f383b2e4d22f76051
Andrew Duncan <ald3nc@mail.missouri.edu>
2017-11-08 09:04:44 -0600

    menu and customer menu

--- src/3220_Final_Project.cpp
+++ src/3220_Final_Project.cpp
@@ -1,15 +1,49 @@
-//============================================================================
-// Name        : 3220_Final_Project.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+int menu();
+
+class Customer{
+private:
+	int choice = 0;
+
+public:
+	Customer();
+	int Customer_Menu();
+
+};
+Customer::Customer(){
+	//enter username and password
+	//open file to check
+	//give option to return to main menu
+}
+
+int Customer::Customer_Menu(){
+	cout << "Enter 1 to view account balance" << endl;			//savings/checking?
+	cout << "Enter 2 to deposit funds" << endl;
+	cout << "Enter 3 to withdraw funds" << endl;
+	return 1;
+}
+
+int menu(){
+	int choice = 0;
+	cout << "Enter 1 to log in as a customer" << endl;		//create customer class
+	cout << "Enter 2 to log in as an employee" << endl;		//employee class
+	cout << "Enter 3 to log in as a manager" << endl;		//manager class
+	cout << "Enter 4 to request new customer account" << endl;		//employee class
+	cout << "Enter 5 to exit" << endl;
+	cin >> choice;			//need to error check choice here
+	return choice;
+}
+
+int main(void){
+	int choice;
+	choice = menu();
+	cout << choice << endl;
+	if(choice == 1){
+		Customer customer1;
+		customer1.Customer_Menu();
+	}
+
 }
