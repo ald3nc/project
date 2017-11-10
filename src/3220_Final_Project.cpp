e980090867e215363b81b9dc45a1340e12b20b02
Andrew Duncan <ald3nc@mail.missouri.edu>
2017-11-10 16:16:52 -0600

    login as customer, view accounts. only need to error check to finish

--- src/3220_Final_Project.cpp
+++ src/3220_Final_Project.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <fstream>
 using namespace std;
 
 int menu();
@@ -9,47 +11,153 @@ class Account{
 private:
 
 protected:
-	vector<int> FileContents;
+	vector<string> names;
+	vector<double> balance;
 
 public:
 	void ViewBalance(int);
-	Account();
+	Account(string, string);
 
 };
+
 class SavingsAccount:Account{
 private:
 
 protected:
 
 public:
+	SavingsAccount(string, string);
 
 };
+SavingsAccount::SavingsAccount(string file_name, string username):Account(file_name, username){
+	cout << "creating savings account" << endl;
+}
 class CheckingAccount:Account{
 private:
 
 protected:
 
 public:
+	CheckingAccount(string, string);
 
 };
+CheckingAccount::CheckingAccount(string file_name, string username):Account(file_name, username){
+	cout << "creating checking account" << endl;
+}
+Account::Account(string file_name, string username){
+	int length = 0;
+	int x = 0;
+	string a;
+	double b;
+	fstream fptr;
+	fptr.open(file_name);
+	if(!fptr.is_open()){
+		cout << "could not open file" << endl;
+	}
+	//cout << file_name << endl;
+	fptr >> length;
+	//cout << length << endl;
+	for(x=0;x<length;x++){
+
+		fptr >> a;
+		names.push_back(a);
+		//cout << names[x] << endl;
+		fptr >> b;
+		balance.push_back(b);
+		//cout << balance[x] << endl;
+	}
+	for(x=0;x<length;x++){
+		if(names[x] == username){
+			cout << "hello " << username << endl;
+			cout << "your balance is: $" << balance[x] << endl;
+		}
+	}
+
+
+
+
+}
 class Customer{
 private:
 	int choice_c = 0;
+	string username;
+	string password;
+	int length = 0;
+	vector<string> names;
+	vector<string> passwords;
 
 public:
 	Customer();
 	int Customer_Menu();
+	int Customer_login();
+	string get_username();
 
 };
+string Customer::get_username(){
+	return username;
+
+}
 Customer::Customer(){
 	//enter username and password
 	//open file to check
 	//give option to return to main menu
 }
+int Customer::Customer_login(){
+	int x = 0;
+	int y = 0;
+	string a;
+	string b;
+	int loop = 0;
+
+	fstream fptr;
+	fptr.open("customers.txt");
+	if(!fptr.is_open()){
+		cout << "could not open file" << endl;
+	}
+	fptr >> length;
+	//cout << length << endl;
+	for(x=0;x<length;x++){
+		fptr >> a;
+		names.push_back(a);
+		fptr >> b;
+		passwords.push_back(b);
+		//cout << names[x] << endl;
+		//cout << passwords[x] << endl;
+	}
+	while(loop == 0){
+		cout << "enter 'exit' as your username to return to main menu" << endl;
+		cout << "enter your username" << endl;
+		cin >> username;
+		if(username == "exit")
+			return 1;
+		cout << "enter your password" << endl;
+		cin >> password;
+
+	for(x=0;x<length;x++){
+		//if(username == "exit"){
+		//	return 1;
+		//}
+		if(names[x] == username && passwords[x] == password){
+			cout << "welcome" << endl;
+			loop = 1;
+			break;
+		}
+		else{
+			cout << "please try again" << endl;
+			break;
+		}
+
+	}
+
+	}
+	return 0;
+
+}
 
 int Customer::Customer_Menu(){
 	cout << "Enter 1 to manage checking account" << endl;			//savings/checking?
 	cout << "Enter 2 to manage savings account" << endl;
+	cout << "Enter 3 to exit menu" << endl;
 	cin >> choice_c;
 	return choice_c;
 
@@ -69,18 +177,60 @@ int menu(){
 int main(void){
 	int choice;
 	int choice_c;
-	choice = menu();
-	cout << choice << endl;
-	if(choice == 1){
-		Customer customer1;
-		choice_c = customer1.Customer_Menu();
-		switch (choice_c){
-		case 1:	//checking
-			break;
-		case 2: //savings funds
-			break;
+	int return_to_main = 0;
+	int thing = 0;
+	int second_loop = 0;
+	string c = "checking.txt";
+	string s = "savings.txt";
+	string username;
 
+	while(return_to_main == 0){
+
+		choice = menu();
+
+		if(choice == 1){
+			Customer customer1;
+			thing = customer1.Customer_login();
+			username = customer1.get_username();
+			if(thing == 1){
+				//return_to_main = thing;
+				continue;
+			}
+			//cout << return_to_main << endl;
+			while(second_loop == 0){
+				choice_c = customer1.Customer_Menu();
+				if(choice_c == 1){
+					CheckingAccount checking(c, username);
+
+				}
+				if(choice_c == 2){
+					SavingsAccount savings(s, username);
+				}
+				if(choice_c == 3){
+					second_loop = 1;
+				}
+			}
+		}
+		if(choice == 5){
+			cout << "thanks for using this random bank" << endl;
+			return 3;
 		}
 	}
 
+	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
